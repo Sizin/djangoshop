c7191eae6fdca149ec774ac76dca2e9d696d7ae2
Sizin <sinanacar.252@gmail.com>
2020-07-02 22:13:57 +0200

    Add Django module named shop

---
+++ shop/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from shop.models import Product, Order, CartItem, LineItem
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display =['id', 'name', 'price']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'date', 'paid']
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'price', 'quantity', 'product']
+
+
+class LineItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'price', 'quantity', 'date_added', 'order']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(CartItem, OrderItemAdmin)
+admin.site.register(LineItem, LineItemAdmin)
